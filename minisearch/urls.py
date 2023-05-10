b3b713c41120b6d6ca4f9f52b17908b887b8c5c1
nick_zhu <nickzhu012@gmail.com>
2023-05-10 15:33:56 +0800

    Setting

--- minisearch/urls.py
+++ minisearch/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'minisearch'
 
 urlpatterns = [
-path('', views.search_index, name='search_view'),
+    path('', views.home, name = 'home'),
+    path('minisearch', views.search_index, name = 'search_view'),
 ]
--- minisearch/views.py
+++ minisearch/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .search_call import search
+from newsapi import NewsApiClient
 
 def search_index(request):
     results = []
@@ -12,3 +13,20 @@ def search_index(request):
         context = {'results': results[1], 'count': results[0], 'search_term':  search_term}
     return render(request,  'index.html',  context)
 
+def home(request):
+    newsapi = NewsApiClient(api_key='867e32f683c34cd2a89ffd7be01ed367')
+    top = newsapi.get_top_headlines(sources='techcrunch')
+
+    l = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    return render(request, 'home.html', context={"mylist": mylist})
