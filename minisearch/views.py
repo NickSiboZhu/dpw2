e3c9945a3713a36f18e31606a474ca760506f578
nick_zhu <nickzhu012@gmail.com>
2023-05-11 20:38:34 +0800

    Setting

--- minisearch/views.py
+++ minisearch/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .search_call import search
+from newsapi import NewsApiClient
 
 
 def search_index(request):
@@ -16,5 +17,24 @@ def search_results(request):
         print(results)
         context = {'results': results[1], 'count': results[0], 'search_term':  search_term}
     return render(request, 'result.html', context)
+
+
+def news(request):
+    newsapi = NewsApiClient(api_key='867e32f683c34cd2a89ffd7be01ed367')
+    top = newsapi.get_top_headlines(sources='techcrunch')
+
+    l = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    return render(request, 'news.html', context={"mylist": mylist})
 
 
